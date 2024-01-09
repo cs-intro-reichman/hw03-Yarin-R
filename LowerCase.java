cfec698af1a42d4f64d19f724f2fd45eb385458b
Yarin R <36561812+Yarin-R@users.noreply.github.com>
2024-01-09 16:02:37 +0200

    added comments to lowercase

--- LowerCase.java
+++ LowerCase.java
@@ -14,9 +14,12 @@ public class LowerCase {
     * Non-letter characters are left as is.
     */
     public static String lowerCase(String s) {
+        // declaring a new empty string which will comtain the original string with lower case characters.
         String loweString = "";
         for (int i = 0; i < s.length(); i++) {
+            // check if a certain character is an upper case character.
             if ((s.charAt(i) >= 'A') && (s.charAt(i) <= 'Z')) {
+                // input the lower case version of the upper cased character.
                 loweString += (char)(s.charAt(i) + 32);
             }
             else {
