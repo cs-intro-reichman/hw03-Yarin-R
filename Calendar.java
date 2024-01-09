2e830fe17d88b19db266971d2883b9c2effecf17
Yarin R <36561812+Yarin-R@users.noreply.github.com>
2024-01-09 15:08:06 +0200

    changed calendar

--- Calendar.java
+++ Calendar.java
@@ -2,11 +2,13 @@
  * Prints the calendars of a given year.
  */
 public class Calendar {	
+	// Starting the calendar on 1/1/1900
 	static int dayOfMonth = 1;   
 	static int month = 1;
-	static int year = 0;
+	static int year = 1900;
+	static int dayOfWeek = 2;     // 1.1.1900 was a Monday
 	static int nDaysInMonth = 31; // Number of days in January
-	
+
 	/** 
 	 * Prints the calendars of all the years in the 20th century. Also prints the  
 	 * number of Sundays that occured on the first day of the month during this period.
@@ -19,20 +21,31 @@ public class Calendar {
 		// Advances the date and the day-of-the-week from 1/1/1900 till 31/12/1999, inclusive.
 	    // Prints each date dd/mm/yyyy in a separate line. If the day is a Sunday, prints "Sunday".
 	    // The following variable, used for debugging purposes, counts how many days were advanced so far.
-	    year = Integer.parseInt(args[0]);
+		int inputYear = Integer.parseInt(args[0]);
 		int yearDays = 0;
-		if (isLeapYear(year)) {
+		if (isLeapYear(inputYear)) {
 			yearDays = 366;
 		}
 		else {
 			yearDays = 365;
 		}
+
+		while (year != inputYear) {
+			advance();
+		}
+
 		int debugDaysCounter = 0;
 
 	    //// Write the necessary initialization code, and replace the condition
 	    //// of the while loop with the necessary condition 
 		while (debugDaysCounter < yearDays) {
-			System.out.println(dayOfMonth + "/" + month + "/" + year);
+			System.out.print(dayOfMonth + "/" + month + "/" + year);
+			if (dayOfWeek == 1) {
+				System.out.println(" Sunday");
+			}
+			else{
+				System.out.println();
+			}
 			advance();
 			debugDaysCounter++;
         }
@@ -55,6 +68,13 @@ public class Calendar {
 		}
 		else {
 			dayOfMonth++;
+		}
+
+		if (dayOfWeek == 7) {
+			dayOfWeek = 1;
+		}
+		else {
+			dayOfWeek++;
 		}
 	 } 
 		 
@@ -86,4 +106,6 @@ public class Calendar {
 			return 31;
 		}
 	}
+
+	// check if 
 }
